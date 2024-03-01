ed49eab15c1191a0e0c0bc4a2bcce5102355b63f
Rhyan Andrade <alerhyan@gmail.com>
2024-02-29 23:28:31 -0300

    projetofeiito

---
+++ index.js
@@ -0,0 +1,43 @@
+let nome = "Rhyan"
+let XP = 5000
+const mensagem = "Você é"
+let nivelFinal = ""
+switch (XP){   
+    case 1000:
+    nivelfinal = "ferro"
+    
+    break
+    case 2000:
+    nivelFinal="bronze"
+    break
+    
+    case 5000:
+    nivelFinal="prata"
+   
+    break
+    case 7000: 
+    nivelFinal="ouro"
+    
+    break
+    case 8000:
+    
+    nivelFinal="platina"
+   
+    break
+    case 9000: 
+    nivelFinal ="ascedente"
+   
+    break
+    case 10.000:
+    nivelFinal ="imortal"
+    
+    break
+    case 11.000:
+    nivelFinal = "Radiante"
+   break
+     
+}
+
+console.log("O " + nome + " está no nivel "+ nivelFinal)
+
+
